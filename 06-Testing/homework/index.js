d3fadc3dcc27e7c4e68ea4eb9a3d33c17a887ddf
soledadpetrino <93164871+soledadpetrino@users.noreply.github.com>
2022-04-30 09:32:00 -0300

    06-testing

--- 06-Testing/homework/index.js
+++ 06-Testing/homework/index.js
@@ -1,18 +1,43 @@
 const express = require('express');
-const app = express();
+const app = express(); /*Se arma la instancia del servidor con express*/
+/*Si quisiera levantar el servidor, tendria que hacerlo en un módulo diferente, 
+no en este archivo, en la que requerimos el app o servidor, y ahi hacemos el app.listen*/
 
-app.use(express.json()); // for parsing application/json
+
+app.use(express.json()); /*(sirve para poder leer la información que llegue por body)*/
+
 
 app.get('/', (req, res) => {
-  res.send({
+  res.status(200).send({        /*agrego el .status(200) para que quede completo, si no coloco nada, por default me indica que es 200*/
     message: 'hola',
   });
 });
 
+app.get('/test', (req, res) => {
+  res.status(200).send({
+    message: 'test',
+  })
+});
+
+app.post('/sum', (req, res) => {
+  res.status(200).send({
+    result: req.body.a + req.body.b
+  });
+})
+
 app.post('/product', (req, res) => {
-  res.send({
-    result: req.body.a / req.body.b,
+  res.status(200).send({
+    result: req.body.a * req.body.b
   });
 });
+
+app.post('/sumArray', (req, res) => {
+  const { array, num } = req.body
+    if(!array || !num) return res.sendStatus(400);
+    else if(!Array.isArray(array) || typeof num !== 'number') return res.sendStatus(400);                                             /*El método Array.isArray() determina si el valor pasado es un Array*/
+    else return res.sendStatus(200);
+  })
+
+
 
 module.exports = app; // Exportamos app para que supertest session la pueda ejecutar
--- 06-Testing/homework/tests/index.spec.js
+++ 06-Testing/homework/tests/index.spec.js
@@ -1,28 +1,35 @@
 const session = require('supertest-session');
 const app = require('../index.js'); // Importo el archivo de entrada del server de express.
 
-const agent = session(app);
+const agent = session(app); /*para hacer las peticiones*/
 
 describe('Test de APIS', () => {
+
+  /*agent seria la petición, en este caso se pide que el get / responda con un 200,
+  por default, en index.js, cuando hago el app.get, lo toma como un 200 */
+  /*Tener cuidado en este caso, en el segundo test unitario, al hacer peticiones, no 
+  se escriben llaves despues de la arrow function, la idea es que se retorne el agent, 
+  o sea se retorna directamente la petición al servidor, */
+
   describe('GET /', () => {
-    it('responds with 200', () => agent.get('/').expect(200));
-    it('responds with and object with message `hola`', () =>
-        agent.get('/').then((res) => {
+    it('responds with 200', () => agent.get('/').expect(200));                         /*test unitario*/
+    it('responds with and object with a property `message` and value `hola`', () =>    /*test unitario*/
+        agent.get('/').then((res) => {                                                 /*se hace la peticion y se captura la respuesta*/
           expect(res.body.message).toEqual('hola');
         }));
   });
 
   describe('GET /test', () => {
     it('responds with 200', () => agent.get('/test').expect(200));
-    it('responds with and object with message `test`', () =>
+    it('responds with and object with a property `message` and value `test`', () =>
       agent.get('/test').then((res) => {
-        expect(res.body.message).toEqual('hola');
+        expect(res.body.message).toEqual('test'); /*En este caso, modificamos el 'hola' por 'test' para que tenga sentido con lo que hay antes*/
       }));
   });
 
   describe('POST /sum', () => {
     it('responds with 200', () => agent.post('/sum').expect(200));
-    it('responds with the sum of 2 and 3', () =>
+    it('receive from body two params `a` and `b`, then responds with an objects with property `result` and value the sum of `a` and `b`', () =>
       agent.post('/sum')
         .send({a: 2, b: 3})
         .then((res) => {
@@ -31,9 +38,9 @@ describe('Test de APIS', () => {
     );
   });
 
-  describe('POST /producto', () => {
+  describe('POST /product', () => {
     it('responds with 200', () => agent.post('/product').expect(200));
-    it('responds with the product of 2 and 3', () =>
+    it('receive from body two params `a` and `b`, the responds with an object with property `result` and value the product of `a` and `b`', () =>
       agent.post('/product')
         .send({a: 2, b: 3})
         .then((res) => {
@@ -43,14 +50,23 @@ describe('Test de APIS', () => {
   });
 
   describe('POST /sumArray', () => {
-    it('responds with 200', () => agent.get('/test').expect(200));
-    it('responds with and object with message `test`', () =>
+    it('responds with 200', () => agent.post('/sumArray').send({ array: [1], num: 1 }).expect(200));
+    
+    it('responds with 400, if parameters are not correctly sent', () => agent.post('/sumArray').expect(400));
+
+    it('responds with 400, if given parameter `array` is not an array or the given parameter `num` is not a number', () => agent.post('/sumArray').send({ array: 'string', num: 'string'}).expect(400));
+    /*it('It should return true if the sum of two numbers belonging to the array is equal to `num`', () =>
       agent.post('/sumArray')
         .send({array: [2,5,7,10,11,15,20], num: 13})
         .then((res) => {
-          expect(res.body.result).toEqual(true);
+         expect(res.body.result).toEqual(true);
       }));
+    it('It should return false if the sum of two numbers belonging to the array is not equal to `num`', () =>
+      agent.post('/sumArray')
+        .send({array: [2,5,7,10,11,15,20], num: 99})
+        .then((res) => {
+          expect(res.body.result).toEqual(false);
+      }));*/
   });
 
 });
-
